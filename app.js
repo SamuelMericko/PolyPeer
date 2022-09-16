9fc32116622465899b94c7492fc37b9af2201a81
Loganded <mericko.samuel@gmail.com>
2022-09-16 16:47:32 +0200

    pripojenie k databáze a vytvorenie routerov

--- app.js
+++ app.js
@@ -5,10 +5,26 @@ const helmet = require('helmet');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 
+// Routre
+const userRoute = require('./routes/pouzivatelia');
+
 // inicializácia aplikácie
 const app = express();
 dotenv.config();
 
+// Pripojenie k databáze
+mongoose.connect(process.env.MONHO_URL, {useNewUrlParser: true}, () => {
+    console.log('pripojený k MongoDB');
+});
+
+// Middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'));
+
+app.use('/api/pouzivatelia', userRoute);
+
+// Aplikácia
 app.listen(8800,() => {
     console.log('backend server beží');
 });
