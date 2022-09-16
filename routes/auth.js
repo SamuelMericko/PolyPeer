a5d92e04f604d8f1199b376e8e8622f41c38a5e5
Loganded <mericko.samuel@gmail.com>
2022-09-16 17:36:12 +0200

    registračný systém 3

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,22 @@
 const router = require('express').Router();
 const Pouzivatel = require('../models/Pouzivatelia');
+const bcrypt = require('bcrypt');
 
 
 // Registrácia
 router.post('/registracia', async (req, res) => {
-    const novyPouzivatel = new Pouzivatel({
-        meno: req.body.meno,
-        email: req.body.email,
-        heslo: req.body.heslo
-    });
-
     try {
+        // Vytvorenie hashovaného hesla
+        const salt = await bcrypt.genSalt(10);
+        const hashHeslo = await bcrypt.hash(req.body.heslo, salt);
+            
+        // Vytvorenie nového používateľa
+        const novyPouzivatel = new Pouzivatel({
+            meno: req.body.meno,
+            email: req.body.email,
+            heslo: hashHeslo
+        });    
+            
         const pouzivatel = await novyPouzivatel.save();
         res.status(200).json(pouzivatel);
     } catch(err) {
