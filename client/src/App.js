7393ab1f13bf192c2555a6c686952832881a14dd
Samuel Meričko <mericko.samuel@gmail.com>
2022-10-12 22:00:46 +0200

    login-page-final1

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,31 @@
+import React from "react";
 import Prihlasenie from "./pages/Prihlasenie/Prihlasenie";
+import HashLoader from 'react-spinners/HashLoader';
+import { useEffect, useState } from "react";
+import './App.css';
+
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+      setLoading(true);
+      setTimeout(() => {
+          setLoading(false);
+      }, 2000);
+  }, []);
+
   return (
-    <Prihlasenie />
+    <div className="App">
+    {loading ? (
+      <>
+      <img src="/images/logo.svg" alt="Polypeer_logo" className="logo" width="40%"/>
+      <HashLoader color="#00B0E6" size={100} className="loader" />
+      </>
+    ) : (
+      <Prihlasenie />
+    )}
+    </div>
   );
 }
 
