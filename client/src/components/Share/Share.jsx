c228fe8d5db393c9d3dc5b0ec6eca32aa9e5a641
Samuel Meričko <mericko.samuel@gmail.com>
2022-10-24 19:46:22 +0200

    dizajn edit

--- client/src/components/Share/Share.jsx
+++ client/src/components/Share/Share.jsx
@@ -12,25 +12,25 @@ const Share = () => {
                 </div>
                 <hr className="shareHr"/>
                 <div className="shareBottom">
-                <div className="shareOptions">
-                    <div className="shareOption">
-                        <PermMedia htmlColor="tomato" className="shareIcon"/>
-                        <span className="shareOptionText">Photo / Video</span>
+                    <div className="shareOptions">
+                        <div className="shareOption">
+                            <PermMedia htmlColor="tomato" className="shareIcon"/>
+                            <span className="shareOptionText">Photo / Video</span>
+                        </div>
+                        <div className="shareOption">
+                            <Label htmlColor="blue" className="shareIcon"/>
+                            <span className="shareOptionText">Tag</span>
+                        </div>
+                        <div className="shareOption">
+                            <Room htmlColor="green" className="shareIcon"/>
+                            <span className="shareOptionText">Lokácia</span>
+                        </div>
+                        <div className="shareOption">
+                            <EmojiEmotions htmlColor="goldenrod" className="shareIcon"/>
+                            <span className="shareOptionText">Emoji</span>
+                        </div>
                     </div>
-                    <div className="shareOption">
-                        <Label htmlColor="blue" className="shareIcon"/>
-                        <span className="shareOptionText">Tag</span>
-                    </div>
-                    <div className="shareOption">
-                        <Room htmlColor="green" className="shareIcon"/>
-                        <span className="shareOptionText">Lokácia</span>
-                    </div>
-                    <div className="shareOption">
-                        <EmojiEmotions htmlColor="goldenrod" className="shareIcon"/>
-                        <span className="shareOptionText">Emoji</span>
-                    </div>
-                </div>
-                <button className="shareButton">Odoslať</button>
+                    <button className="shareButton">Odoslať</button>
                 </div>
             </div>
         </div>
