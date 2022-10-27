8cb9866f7078e7df8891f9dfd25d1c9fb4f45570
Samuel Meričko <mericko.samuel@gmail.com>
2022-10-27 15:30:47 +0200

    login progress

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -2,27 +2,35 @@ import React from "react";
 import './Login.css'
 import { Person, Lock, ArrowUpward} from '@mui/icons-material';
 import Button from '@mui/material/Button';
+import { useRef } from "react";
 
 const Login = () => {
+    const email = useRef();
+    const heslo = useRef();
+    const handleClick = (e) => {
+        e.preventDefault();
+        console.log(email.current.value);
+    }
+
     return (
         <div className="login">
             <img src="/images/logo.svg" alt="Polypeer_logo" className="loginLogo" width="70%" />
 
-            <div className="loginForm">
+            <form className="loginForm" onSubmit={handleClick}>
                 <div className="loginFormPole">
                     <Person className="loginIcon"/>
-                    <input type="text" placeholder="Prihlasovacie meno" />
+                    <input type="email" placeholder="Email" ref={email} required/>
                 </div>
                 <div className="loginFormPole">
                     <Lock className="loginIcon"/>
-                    <input type="password" placeholder="Heslo" />
+                    <input type="password" placeholder="Heslo" ref={heslo} minLength={8} required/>
                     <br />
                 </div>
                 <br />
                 <span> <a href="/login" className="loginZabudnuteHeslo">Zabudli ste heslo?</a></span>
                 <br />
                 <Button variant="contained" type="submit" endIcon={<ArrowUpward />} size="large" className="loginSubmitButton">Odoslať</Button>
-            </div>
+            </form>
         </div>
     );
 }
