d4bfc11a585e2f070b8474f22177835bdd089ff7
Loganded <mericko.samuel@gmail.com>
2022-11-01 18:54:08 +0100

    socket io complete?

--- client/src/pages/Chat/Chat.jsx
+++ client/src/pages/Chat/Chat.jsx
@@ -12,69 +12,97 @@ import Sprava from "../../components/Sprava/Sprava";
 import ChatOnline from "../../components/ChatOnline/ChatOnline";
 
 const Chat = () => {
-    const {user} = useContext(AuthContext);
     const [conversations, setConversations] = useState([]);
     const [currentChat, setCurrentChat] = useState(null);
     const [messages, setMessages] = useState([]);
     const [newMessage, setNewMessage] = useState("");
+    const [arrivalMessage, setArrivalMessage] = useState(null);
+    const [onlineUsers, setOnlineUsers] = useState([]);
     const socket = useRef();
+    const { user } = useContext(AuthContext);
     const scrollRef = useRef();
-
+  
     useEffect(() => {
-        socket.current = io('ws://localhost:8900');
-    },[]);
-
-    useEffect(()=> {
-        socket.current.emit('addUser', user._id);
-        socket.current.on('getUsers', users => {
-            console.log(users);
+      socket.current = io("ws://localhost:8900");
+      socket.current.on("getMessage", (data) => {
+        setArrivalMessage({
+          sender: data.senderId,
+          text: data.text,
+          createdAt: Date.now(),
         });
-    }, [user]);
-
+      });
+    }, []);
+  
     useEffect(() => {
-        const getConversations = async () => {
-          try {
-            const res = await axios.get("/conversations/" + user._id);
-            setConversations(res.data);
-          } catch (err) {
-            console.log(err);
-          }
-        };
-        getConversations();
-      }, [user]);
-
-      useEffect(() => {
-        const getMessages = async () => {
-          try {
-            const res = await axios.get("/messages/" + currentChat?._id);
-            setMessages(res.data);
-          } catch (err) {
-            console.log(err);
-          }
-        };
-        getMessages();
-      }, [currentChat]);
-
-      const handleSubmit = async (e) => {
-        e.preventDefault();
-        const message = {
-            sender: user._id,
-            text: newMessage,
-            conversationId: currentChat._id
-        };
-
+      arrivalMessage &&
+        currentChat?.members.includes(arrivalMessage.sender) &&
+        setMessages((prev) => [...prev, arrivalMessage]);
+    }, [arrivalMessage, currentChat]);
+  
+    useEffect(() => {
+      socket.current.emit("addUser", user._id);
+      socket.current.on("getUsers", (users) => {
+        setOnlineUsers(
+          user.followings.filter((f) => users.some((u) => u.userId === f))
+        );
+      });
+    }, [user]);
+  
+    useEffect(() => {
+      const getConversations = async () => {
         try {
-            const res = await axios.post('/messages', message);
-            setMessages([...messages, res.data])
-            setNewMessage("");
-        } catch(err) {
-            console.log(err);
+          const res = await axios.get("/conversations/" + user._id);
+          setConversations(res.data);
+        } catch (err) {
+          console.log(err);
         }
       };
-
-      useEffect(() => {
-        scrollRef.current?.scrollIntoView({behavior: "smooth"});
-      }, [messages])
+      getConversations();
+    }, [user._id]);
+  
+    useEffect(() => {
+      const getMessages = async () => {
+        try {
+          const res = await axios.get("/messages/" + currentChat?._id);
+          setMessages(res.data);
+        } catch (err) {
+          console.log(err);
+        }
+      };
+      getMessages();
+    }, [currentChat]);
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      const message = {
+        sender: user._id,
+        text: newMessage,
+        conversationId: currentChat._id,
+      };
+  
+      const receiverId = currentChat.members.find(
+        (member) => member !== user._id
+      );
+  
+      socket.current.emit("sendMessage", {
+        senderId: user._id,
+        receiverId,
+        text: newMessage,
+      });
+  
+      try {
+        const res = await axios.post("/messages", message);
+        setMessages([...messages, res.data]);
+        setNewMessage("");
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  
+    useEffect(() => {
+      scrollRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, [messages]);
+  
 
     return (
         <>
