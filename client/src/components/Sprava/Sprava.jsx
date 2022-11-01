98884b348b3bf97d51fdb1807142875a6c1f2378
Loganded <mericko.samuel@gmail.com>
2022-11-01 18:26:32 +0100

    socket progress

--- client/src/components/Sprava/Sprava.jsx
+++ client/src/components/Sprava/Sprava.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import './Sprava.css';
+import {format} from 'timeago.js';
 
-const Sprava = ({vlastna}) => {
+const Sprava = ({message, vlastna}) => {
     return (
         <div className={vlastna  ? "sprava vlastna" : "sprava"}>
             <div className="spravaHore">
                 <img className="spravaImg" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwritestylesonline.com%2Fwp-content%2Fuploads%2F2016%2F08%2FFollow-These-Steps-for-a-Flawless-Professional-Profile-Picture.jpg&f=1&nofb=1&ipt=05b95ab2eb10727742e3923f1c0329b34351893e42049949a39823e6d2840032&ipo=images" alt="" />
-                <p className="spravaText">toto je správa</p>
+                <p className="spravaText">{message.text}</p>
             </div> 
             <div className="spravaDole">
-                Pred 1 hodinou
+                {format(message.createdAt)}
             </div>
         </div>
     );
--- client/src/pages/Chat/Chat.jsx
+++ client/src/pages/Chat/Chat.jsx
@@ -1,21 +1,35 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import './Chat.css';
 import { Button } from "@mui/material";
+import { AuthContext } from "../../context/AuthContext";
+import axios from "axios";
+import {io} from 'socket.io-client';
 
 // Komponenty
 import Topbar from '../../components/Topbar/Topbar';
 import Konverzacia from "../../components/Konverzacia/Konverzacia";
 import Sprava from "../../components/Sprava/Sprava";
 import ChatOnline from "../../components/ChatOnline/ChatOnline";
-import { AuthContext } from "../../context/AuthContext";
-import axios from "axios";
-
 
 const Chat = () => {
     const {user} = useContext(AuthContext);
     const [conversations, setConversations] = useState([]);
     const [currentChat, setCurrentChat] = useState(null);
     const [messages, setMessages] = useState([]);
+    const [newMessage, setNewMessage] = useState("");
+    const socket = useRef();
+    const scrollRef = useRef();
+
+    useEffect(() => {
+        socket.current = io('ws://localhost:8900');
+    },[]);
+
+    useEffect(()=> {
+        socket.current.emit('addUser', user._id);
+        socket.current.on('getUsers', users => {
+            console.log(users);
+        });
+    }, [user]);
 
     useEffect(() => {
         const getConversations = async () => {
@@ -27,8 +41,40 @@ const Chat = () => {
           }
         };
         getConversations();
-      }, [user._id]);
-    
+      }, [user]);
+
+      useEffect(() => {
+        const getMessages = async () => {
+          try {
+            const res = await axios.get("/messages/" + currentChat?._id);
+            setMessages(res.data);
+          } catch (err) {
+            console.log(err);
+          }
+        };
+        getMessages();
+      }, [currentChat]);
+
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+        const message = {
+            sender: user._id,
+            text: newMessage,
+            conversationId: currentChat._id
+        };
+
+        try {
+            const res = await axios.post('/messages', message);
+            setMessages([...messages, res.data])
+            setNewMessage("");
+        } catch(err) {
+            console.log(err);
+        }
+      };
+
+      useEffect(() => {
+        scrollRef.current?.scrollIntoView({behavior: "smooth"});
+      }, [messages])
 
     return (
         <>
@@ -38,33 +84,31 @@ const Chat = () => {
                     <div className="chatMenuWrapper">
                         <input type="text" placeholder="Nájdite svojich priateľov" className="chatMenuInput"/>
                         {conversations.map(c => (
-                            <Konverzacia conversation={c} currentUser={user}/>
+                            <div onClick={() => setCurrentChat(c)}>
+                                <Konverzacia conversation={c} currentUser={user}/>
+                            </div>
                         ))}
                     </div>
                 </div>
                 <div className="chatBox">
                     <div className="chatBoxWrapper">
-                        <div className="chatBoxHore">
-                            <Sprava />
-                            <Sprava vlastna={true} />
-                            <Sprava />
-                            <Sprava />
-                            <Sprava vlastna={true} />
-                            <Sprava />
-                            <Sprava />
-                            <Sprava vlastna={true} />
-                            <Sprava />
-                            <Sprava />
-                            <Sprava vlastna={true} />
-                            <Sprava />
-                            <Sprava />
-                            <Sprava vlastna={true} />
-                            <Sprava />
-                        </div>
-                        <div className="chatBoxDole">
-                            <textarea className="chatSpravaInput" placeholder="Napíšte niečo..."></textarea>
-                            <Button className="chatOdoslat" variant="contained">Odoslať</Button>
-                        </div>
+                        {currentChat ? (
+                        <>
+                            <div className="chatBoxHore">
+                                {messages.map(m=>(
+                                    <div ref={scrollRef}>
+                                        <Sprava message={m} vlastna={m.sender === user._id}/>
+                                    </div>
+                                ))}
+                            </div>
+                            <div className="chatBoxDole">
+                                <textarea className="chatSpravaInput" placeholder="Napíšte niečo..." onChange={(e) => setNewMessage(e.target.value)} value={newMessage}></textarea>
+                                <Button className="chatOdoslat" variant="contained" onClick={handleSubmit}>Odoslať</Button>
+                            </div>
+                        </>
+                        ):(
+                            <p className="noConversationText">Ak chcete začať chatovať, kliknite na konverzáciu</p>
+                        )}
                     </div>
                 </div>
                 <div className="chatOnline">
